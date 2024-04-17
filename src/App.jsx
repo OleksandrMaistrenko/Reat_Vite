7a0d3087e327c5c65d4a4cc8b2aa60f23214c868
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-04-17 16:09:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ShowText from "./text/ShowText";
+
+function App() {
+  return (
+    <>
+      <ShowText />
+    </>
+  );
+}
+
+export default App;
---
+++ src/text/ShowText.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShowText = () => {
+  return (
+    <div>
+      <h1>Hello React Vite</h1>
+    </div>
+  );
+};
+
+export default ShowText;
